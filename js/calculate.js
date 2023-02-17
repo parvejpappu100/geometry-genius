5019669d31f34c84b16f3d25bb82af0204417b6e
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-02-18 02:53:51 +0600

    added rhombus,prntagon and ellipse calculation by javascript

--- js/calculate.js
+++ js/calculate.js
@@ -49,3 +49,53 @@ document.getElementById("calculate-parallelogram").addEventListener("click",func
     setTextElementValue("result-parallelogram",finalResult);
 });
 
+// * Rhombus:
+// * set add event listener to the Rhombus calculate btn:
+document.getElementById("calculate-rhombus").addEventListener("click",function(){
+    // * Get the value of d1 from the input field:
+    const rhombusD1Total = getInputElementValueById("rhombus-d1-total");
+    // * set the value of d1:
+    setTextElementValue("rhombus-d1",rhombusD1Total);
+    // * Get the value of d2 from the input field:
+    const rhombusD2Total = getInputElementValueById("rhombus-d2-total");
+    // * set the value of d2:
+    setTextElementValue("rhombus-d2",rhombusD2Total);
+    // * Get the final result:
+    const finalResult = 0.5 * rhombusD1Total * rhombusD2Total;
+    // * set the final result in rhombus calculation area:
+    setTextElementValue("result-rhombus",finalResult);
+});
+
+// * Pentagon:
+// * set add event listener to the Pentagon calculate btn:
+document.getElementById("calculate-pentagon").addEventListener("click",function(){
+    // * Get the range value:
+    const rangeValue = getInputElementValueById("pentagon-range-total");
+    // * set the value of range in p:
+    setTextElementValue("range",rangeValue);
+    // * get the value of perpendicular distance :
+    const perpendicularDistanceTotal = getInputElementValueById("perpendicular-distance-total");
+    // * set perpendicular-distance-total value into perpendicular-distance:
+    setTextElementValue("perpendicular-distance",perpendicularDistanceTotal);
+    // * get final resut:
+    const finalResult = 0.5 * rangeValue * perpendicularDistanceTotal;
+    // * set the final result:
+    setTextElementValue("result-pentagon",finalResult);
+});
+
+// * Ellipse:
+// * set add event listener to the Ellipse calculate btn:
+document.getElementById("calculate-ellipse").addEventListener("click",function(){
+    // * get the value of a from the input field:
+    const valueOfA = getInputElementValueById("value-of-a-field");
+    // * set the valueOfA into a:
+    setTextElementValue("value-of-a",valueOfA);
+    // * get the value of b from the input field:
+    const valueOfB = getInputElementValueById("value-of-b-field");
+    // * set the valueOfB into b:
+    setTextElementValue("value-of-b",valueOfB);
+    // * get the final result:
+    const finalResult =  3.14 * valueOfA * valueOfB;
+    // * set the final result:
+    setTextElementValue("result-ellipse",finalResult);
+});
