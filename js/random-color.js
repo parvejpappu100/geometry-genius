6b774437f52e8c12f18ee4a0082caa98d4d7aeb0
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-02-18 17:17:36 +0600

    added random background color of penragon card

--- js/random-color.js
+++ js/random-color.js
@@ -20,4 +20,20 @@ document.getElementById("parallelogram-card").addEventListener("mouseenter",func
 });
 document.getElementById("parallelogram-card").addEventListener("mouseleave",function(){
     getPreviousColor("parallelogram-card");
-})
+});
+
+// * add random background color of rhombus card:
+document.getElementById("rhombus-card").addEventListener("mouseenter",function(){
+    setRandomColor('rhombus-card');
+});
+document.getElementById("rhombus-card").addEventListener('mouseleave',function(){
+    getPreviousColor("rhombus-card");
+});
+
+// * add random background color of Pentagon card:
+document.getElementById("pentagon-card").addEventListener("mouseenter",function(){
+    setRandomColor('pentagon-card');
+});
+document.getElementById("pentagon-card").addEventListener("mouseleave",function(){
+    getPreviousColor("pentagon-card");
+});
