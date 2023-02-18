2eaed3af58bdae89dfb559bf0405704c1f7f35c1
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-02-18 13:28:49 +0600

    added validation

--- js/utilities.js
+++ js/utilities.js
@@ -3,6 +3,11 @@ function getInputElementValueById(elementId){
     const element = document.getElementById(elementId);
     const elementValueString = element.value ;
     const elementValue = parseFloat(elementValueString);
+    element.value ="";
+    if(isNaN(elementValue)){
+        alert("Please provide a number.")
+        return getInputElementValueById();
+    }
     return elementValue;
 };
 
